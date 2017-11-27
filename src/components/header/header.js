5de177a9960211557d39190de46faab0c2045932
Eugene <hadanyonok@gmail.com>
2017-11-27 03:20:06 +0300

    links added

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,16 +1,23 @@
 import React from 'react';
-import { Box, Header, Title } from 'grommet';
+import { Box, Header, Title, Anchor } from 'grommet';
 import './header.css';
 
 const HeaderComponent = () => (
-  <Header separator="bottom" colorIndex="light-2-a" justify="center">
+  <Header
+    separator="bottom"
+    colorIndex="light-2-a"
+    justify="center"
+    size="small"
+  >
     <Box
       direction="row"
       flex="grow"
       pad={{ horizontal: 'medium' }}
       responsive={false}
     >
-      <Title className="header-title">SNEAKERSEARCH</Title>
+      <Title className="header-title">
+        <Anchor path="/">SNEAKERSEARCH</Anchor>
+      </Title>
     </Box>
   </Header>
 );

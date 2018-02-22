18e83869a613b293ca6e17dbc90bb1971e61980d
Eugene <hadanyonok@gmail.com>
2018-02-22 18:03:28 +0300

    screens filestructure update

---
+++ src/screens/catalog.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import { parse } from 'qs';
+import { Article, Heading, Anchor, Section } from 'grommet';
+
+import FilterComponent from '../components/filter';
+import Tiles from '../components/tiles';
+import CatalogPageTile from '../components/tiles/catalog-page_tile';
+
+import { loadProducts } from '../ducks/products';
+
+class CatalogPage extends React.Component {
+  constructor(props) {
+    super(props);
+    const { location: { search } } = props;
+    this.state = { values: parse(search.substring(1)) };
+  }
+
+  componentDidMount() {
+    this.props.loadProducts(this.state.values);
+  }
+
+  render() {
+    const {
+      filterOnDisplay,
+      loadProducts,
+      products,
+      match: { params },
+    } = this.props;
+
+    let filter;
+    if (filterOnDisplay)
+      filter = (
+        <FilterComponent
+          handleSubmit={loadProducts}
+          amount={products.length}
+          initialValues={this.state.values}
+        />
+      );
+
+    return (
+      <Article
+        className="screen-content"
+        colorIndex="light-2"
+        pad={{ horizontal: 'xlarge' }}
+      >
+        <Heading className="no-margin-bottom" uppercase tag="h4" margin="small">
+          <Anchor path="/">home</Anchor> / catalog
+        </Heading>
+        <Heading uppercase strong align="center" tag="h2">
+          {params.department}
+        </Heading>
+        <Section pad="none" align="start" direction="row" colorIndex="light-2">
+          {filter}
+          <Tiles tile={CatalogPageTile} tiles={products} />
+        </Section>
+      </Article>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  filterOnDisplay: state.filter,
+  products: state.products,
+});
+
+CatalogPage.defaultProps = {
+  products: [],
+};
+
+CatalogPage.propTypes = {
+  location: PropTypes.shape({ search: PropTypes.string.isRequired }).isRequired,
+  loadProducts: PropTypes.func.isRequired,
+  filterOnDisplay: PropTypes.bool.isRequired,
+  products: PropTypes.arrayOf(PropTypes.shape),
+  match: PropTypes.shape({ params: PropTypes.shape.isRequired }).isRequired,
+};
+
+export default connect(mapStateToProps, { loadProducts })(CatalogPage);
--- src/screens/home.js
+++ src/screens/home.js
@@ -3,15 +3,17 @@ import { connect } from 'react-redux';
 
 import { Article } from 'grommet';
 
-import wallpapers from '../../assets/main.jpg';
-import men from '../../assets/men.png';
-import women from '../../assets/women.png';
-import kids from '../../assets/kids.png';
+import ScrollToTopOnMount from '../components/scroll-to-top';
 
-import MainScreenComponent from '../../components/slide-show/slide-show';
-import TilesComponent from '../../components/tiles/tiles';
-import MainPageTile from '../../components/tiles/tile/main-page_tile';
-import ResponsiveCarousel from '../../components/carousel/carousel';
+import wallpapers from '../assets/main.jpg';
+import men from '../assets/men.png';
+import women from '../assets/women.png';
+import kids from '../assets/kids.png';
+
+import MainScreenComponent from '../components/slide-show/slide-show';
+import Tiles from '../components/tiles';
+import MainPageTile from '../components/tiles/home-page_tile';
+import ResponsiveCarousel from '../components/carousel/index';
 
 const mainPageTiles = [
   { key: 'men', heading: 'men', img: men },
@@ -19,11 +21,12 @@ const mainPageTiles = [
   { key: 'kids', heading: 'kids', img: kids },
 ];
 
-const MainPage = () => (
+const HomePage = () => (
   <Article className="screen-content" colorIndex="light-2">
+    <ScrollToTopOnMount />
     <MainScreenComponent wallpapers={wallpapers} />
     <ResponsiveCarousel />
-    <TilesComponent
+    <Tiles
       selectable
       fill
       tile={MainPageTile}
@@ -38,4 +41,4 @@ const mapDispatchToProps = state => ({
   filter: state.form.filter || {},
 });
 
-export default connect(mapDispatchToProps)(MainPage);
+export default connect(mapDispatchToProps)(HomePage);
